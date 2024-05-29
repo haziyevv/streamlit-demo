9cf8fb11865e3af09249c563a4e3927bc7f002a5
Farid Haziyev <ferid.heziyev@hotmail.com>
2024-05-29 12:35:06 +0400

    Update streamlit_naics_detector.py

--- streamlit_naics_detector.py
+++ streamlit_naics_detector.py
@@ -32,7 +32,7 @@ client = OpenAI(
 gpt_user_prompt = """I will give you the name of the company and you will decide the NAICS code id it belongs to, with its description.
 Please return the result in the following format, do not include any explanations:
 {{
-"NAICS code": "NAICS code",
+"NAICS_code": "NAICS code",
 "description": "description of NAICS code"
 }}
 Utilize your expertise to generate the most pertinent information.
